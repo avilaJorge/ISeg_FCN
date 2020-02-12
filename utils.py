4722a33fe0270397d4f4602db7902e86134747e3
Jorge Avila <j3avila@ucsd.edu>
2020-02-12 12:28:22 -0800

    backup commit.

--- utils.py
+++ utils.py
@@ -1,18 +1,28 @@
 import torch
+from dataloader import *
+import numpy as np
 def iou(pred, target):
     ious = []
-    for cls in range(34):
+    cls_ious = []
+    for cls in valid_ids:
+        for i in range(pred.size()[0]):
         # Complete this function
-        intersection = ((pred == cls) & (target == cls)).sum().float()
-        union = ((pred == cls) | (target == cls)).sum().float()
-        if union == 0:
-            ious.append(float('nan'))  # if there is no ground truth, do not include in evaluation
-        else:
+            intersection = ((pred[i] == cls) & (target[i] == cls)).sum().float()
+            union = ((pred[i] == cls) | (target[i] == cls)).sum().float()
+            
+            if union == 0:
+                cls_ious.append(float('nan'))  # if there is no ground truth, do not include in evaluation
+            else:
             # Append the calculated IoU to the list ious
-            ious.append(float(intersection / union))
+                cls_ious.append(float(intersection / union))
+        
+        ious.append(np.nanmean(cls_ious))
     return ious
 
 
 def pixel_acc(pred, target):
     #Complete this function
-    return float((pred == target).sum().float() / pred.nelement())
+    acc_arr = []
+    for i in range(pred.size()[0]):
+        acc_arr.append(float((pred[i] == target[i]).sum().float() / pred[i].nelement()))
+    return np.nanmean(acc_arr)
